3c40d3371c8e3addff7d8f0a71a104c2758fc137
Robinson Betancur Marin <rbetancurm@ultragroupla.com>
2024-03-18 16:06:18 -0500

    show titleof images

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,14 +1,26 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
+  const [images, setImages] = useState([]);
+
   useEffect(() => {
-    getGifs(category);
+    newGifs();
   }, []);
+
+  const newGifs = async () => {
+    const gifs = await getGifs(category);
+    setImages(gifs);
+  };
 
   return (
     <>
       <h3>{category}</h3>
+      <ol>
+        {images.map(({ id, title }) => (
+          <li key={id}>{title}</li>
+        ))}
+      </ol>
     </>
   );
 };
